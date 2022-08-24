293372b2728632cabe2f96eeb67c1db153dbfffa
Dawid Cichy <38354498+Biker5522@users.noreply.github.com>
2022-08-24 09:36:02 +0200

    fix

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('..//Models/UserModel');
+const User = require('../Models/UserModel');
 const jwt = require('jsonwebtoken');
 
 //POST Add User
